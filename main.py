3f9a7c47c06bce833f12d586fa49c72cd99cc810
MyaFryckman22 <817258@mystma.org>
2021-09-28 14:33:42 -0500

    1

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
 #WRITE YOUR CODE IN THIS FILE
+#define function 
+def password(p):
+    if "Knights19":
+        return "ACCESS GRANTED"
+    else:
+        return "ACCESS DENIED"
+
+print(password("alphabet"))
